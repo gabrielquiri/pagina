e5159022c68d3d6dc847418f220e2100083f2d75
gabriel quirino <gquirino448@gmail.com>
2023-05-30 02:30:11 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,188 @@
+let contador = document.getElementById("contador")
+function aumentar() {
+    contador.innerHTML = parseInt(contador.innerText) + 1;
+}
+
+function diminuir() {
+    contador.innerHTML = parseInt(contador.innerText) - 1;
+}
+
+let contador2 = document.getElementById("contador2")
+function aumentar2() {
+    contador2.innerHTML = parseInt(contador2.innerText) + 1;
+}
+
+function diminuir2() {
+    contador2.innerHTML = parseInt(contador2.innerText) - 1;
+}
+
+let contador3 = document.getElementById("contador3")
+function aumentar3() {
+    contador3.innerHTML = parseInt(contador3.innerText) + 1;
+}
+
+function diminuir3() {
+    contador3.innerHTML = parseInt(contador3.innerText) - 1;
+}
+
+let contador4 = document.getElementById("contador4")
+function aumentar4() {
+    contador4.innerHTML = parseInt(contador4.innerText) + 1;
+}
+
+function diminuir4() {
+    contador4.innerHTML = parseInt(contador4.innerText) - 1;
+}
+
+let contador5 = document.getElementById("contador5")
+function aumentar5() {
+    contador5.innerHTML = parseInt(contador5.innerText) + 1;
+}
+
+function diminuir5() {
+    contador5.innerHTML = parseInt(contador5.innerText) - 1;
+}
+
+let contador6 = document.getElementById("contador6")
+function aumentar6() {
+    contador6.innerHTML = parseInt(contador6.innerText) + 1;
+}
+
+function diminuir6() {
+    contador6.innerHTML = parseInt(contador6.innerText) - 1;
+}
+
+let contador7 = document.getElementById("contador7")
+function aumentar7() {
+    contador7.innerHTML = parseInt(contador7.innerText) + 1;
+}
+
+function diminuir7() {
+    contador7.innerHTML = parseInt(contador7.innerText) - 1;
+}
+
+let contador8 = document.getElementById("contador8")
+function aumentar8() {
+    contador8.innerHTML = parseInt(contador8.innerText) + 1;
+}
+
+function diminuir8() {
+    contador8.innerHTML = parseInt(contador8.innerText) - 1;
+}
+
+let contador9 = document.getElementById("contador9")
+function aumentar9() {
+    contador9.innerHTML = parseInt(contador9.innerText) + 1;
+}
+
+function diminuir9() {
+    contador9.innerHTML = parseInt(contador9.innerText) - 1;
+}
+
+let contador10 = document.getElementById("contador10")
+function aumentar10() {
+    contador10.innerHTML = parseInt(contador10.innerText) + 1;
+}
+
+function diminuir10() {
+    contador10.innerHTML = parseInt(contador10.innerText) - 1;
+}
+
+let contador11 = document.getElementById("contador11")
+function aumentar11() {
+    contador11.innerHTML = parseInt(contador11.innerText) + 1;
+}
+
+function diminuir11() {
+    contador11.innerHTML = parseInt(contador11.innerText) - 1;
+}
+
+let contador12 = document.getElementById("contador12")
+function aumentar12() {
+    contador12.innerHTML = parseInt(contador12.innerText) + 1;
+}
+
+function diminuir12() {
+    contador12.innerHTML = parseInt(contador12.innerText) - 1;
+}
+
+let contador13 = document.getElementById("contador13")
+function aumentar13() {
+    contador13.innerHTML = parseInt(contador13.innerText) + 1;
+}
+
+function diminuir13() {
+    contador13.innerHTML = parseInt(contador13.innerText) - 1;
+}
+
+let contador14 = document.getElementById("contador14")
+function aumentar14() {
+    contador14.innerHTML = parseInt(contador14.innerText) + 1;
+}
+
+function diminuir14() {
+    contador14.innerHTML = parseInt(contador14.innerText) - 1;
+}
+
+let contador15 = document.getElementById("contador15")
+function aumentar15() {
+    contador15.innerHTML = parseInt(contador15.innerText) + 1;
+}
+
+function diminuir15() {
+    contador15.innerHTML = parseInt(contador15.innerText) - 1;
+}
+
+let contador16 = document.getElementById("contador16")
+function aumentar16() {
+    contador16.innerHTML = parseInt(contador16.innerText) + 1;
+}
+
+function diminuir16() {
+    contador16.innerHTML = parseInt(contador16.innerText) - 1;
+}
+
+let contador17 = document.getElementById("contador17")
+function aumentar17() {
+    contador17.innerHTML = parseInt(contador17.innerText) + 1;
+}
+
+function diminuir17() {
+    contador17.innerHTML = parseInt(contador17.innerText) - 1;
+}
+
+let contador18 = document.getElementById("contador18")
+function aumentar18() {
+    contador18.innerHTML = parseInt(contador18.innerText) + 1;
+}
+
+function diminuir18() {
+    contador18.innerHTML = parseInt(contador18.innerText) - 1;
+}
+
+let contador19 = document.getElementById("contador19")
+function aumentar19() {
+    contador19.innerHTML = parseInt(contador19.innerText) + 1;
+}
+
+function diminuir19() {
+    contador19.innerHTML = parseInt(contador19.innerText) - 1;
+}
+
+let contador20 = document.getElementById("contador20")
+function aumentar20() {
+    contador20.innerHTML = parseInt(contador20.innerText) + 1;
+}
+
+function diminuir20() {
+    contador20.innerHTML = parseInt(contador20.innerText) - 1;
+}
+
+let contador21 = document.getElementById("contador21")
+function aumentar21() {
+    contador21.innerHTML = parseInt(contador21.innerText) + 1;
+}
+
+function diminuir21() {
+    contador21.innerHTML = parseInt(contador21.innerText) - 1;
+}
